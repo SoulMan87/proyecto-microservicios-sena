b427f15536dce10e40223f48d8f8bad2e9a625e1
jonathan <jonathanhinestroza@gmail.com>
2020-11-04 12:34:09 -0500

    refactoring

--- microservicios-cursos/src/main/java/com/soulrebel/microservicio/app/cursos/models/repository/CursoRepository.java
+++ microservicios-cursos/src/main/java/com/soulrebel/microservicio/app/cursos/models/repository/CursoRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface CursoRepository extends PagingAndSortingRepository<Curso, Long> {
 
-    @Query("select c from Curso join fetch c.alumnos a where a.id=?1")
+    @Query("select c from Curso c join fetch c.alumnos a where a.id=?1")
     public Curso findCursoByAlumnosId(Long id);
 }
