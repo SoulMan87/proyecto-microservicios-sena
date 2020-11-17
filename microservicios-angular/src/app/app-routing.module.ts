d42056b35cf8e929dd75208f82949719fe0c080c
jonathan <jonathanhinestroza@gmail.com>
2020-11-16 21:27:42 -0500

    components

--- microservicios-angular/src/app/app-routing.module.ts
+++ microservicios-angular/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AlumnosComponent} from './components/alumnos/alumnos.component';
+import {CursosComponent} from './components/cursos/cursos.component';
+import {ExamenesComponent} from './components/examenes/examenes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/alumnos', component: AlumnosComponent},
+  {path: '/curso', component: CursosComponent},
+  {path: '/examenes', component: ExamenesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
