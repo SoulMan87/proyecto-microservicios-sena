5b5326ceb172b8010ae9ec8b63b0803fbaac62ec
jonathan <jonathanhinestroza@gmail.com>
2020-11-19 16:08:37 -0500

    generics refactoring

--- microservicios-angular/src/app/services/examen.service.ts
+++ microservicios-angular/src/app/services/examen.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {CommonService} from './common.service';
+import {Examen} from '../models/examen';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ExamenService {
+export class ExamenService extends CommonService<Examen> {
 
-  constructor() { }
+  protected baseEndpoint = '/api/examenes';
+
 }
+
